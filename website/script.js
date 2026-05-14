a55e9dc4b71f6d5444767755c2e10902b49afda6
Mohana priya R <mohanapriya162020@gmail.com>
2026-05-14 17:29:12 +0530

    Delete website directory

--- website/script.js
+++
@@ -1,5 +0,0 @@
-function enquire(product) {
-    let msg = `Hi Chennai Tools, I need ${product} spare parts`;
-    let url = `https://wa.me/919543895438?text=${encodeURIComponent(msg)}`;
-    window.open(url, "_blank");
-}
